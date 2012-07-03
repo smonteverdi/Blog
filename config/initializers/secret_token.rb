03643c4b0850076f2db21de147dbf867791ec874
Sean Monteverdi <sean@rainworldinteractive.com>
2012-07-02 20:41:48 -0700

    Added Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RwBlog::Application.config.secret_token = '37a19a5b5210732a23d8f093636c2bd4ffb01589f3aaa81c09291e67f257b6fe8fe43cab578733cee37da106f0a872396b24dbac0669517e4bae47ab7eaed2cb'

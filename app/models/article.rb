0f70c73a076a3db967ccaf6f1ed7da8f627f7ed5
Sean Monteverdi <sean@rainworldinteractive.com>
2012-07-14 10:50:39 -0700

    Added comments and created associations with articles and users

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,7 @@ class Article < ActiveRecord::Base
 
   belongs_to :user
   has_and_belongs_to_many :categories
+  has_many :comments
 
   def long_title
   	"#{title} - #{published_at}"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120713045647) do
+ActiveRecord::Schema.define(:version => 20120714170516) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -25,12 +25,21 @@ ActiveRecord::Schema.define(:version => 20120713045647) do
   end
 
   create_table "articles_categories", :id => false, :force => true do |t|
-    t.integer "articles_id"
+    t.integer "article_id"
     t.integer "category_id"
   end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
